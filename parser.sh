92526586ecde985dcacced4c90f07c4a248589f6
qjsoq <artamonovdima355@gmail.com>
2025-04-03 08:46:30 +0000

    Update parser.sh

--- parser.sh
+++ parser.sh
@@ -4,26 +4,33 @@ path_file=$1
 
 process_pids() {
     local pid_array=($(awk '/\[pid/{print $5}' "$path_file" | sed 's/]//' | sort -u))
+    local valid_pids=()
+    local invalid_pids=()
     
     for i in "${pid_array[@]}"; do
         local temp_pid_info=$(awk "/$i/" "$path_file")
-        if [[ "$temp_pid_info" =~ (ERROR|Failed|Timeout) ]]; then
-            pid_array=("${pid_array[@]/$i}")  
-            echo "Pid $i Failed"
+        if [[ "$temp_pid_info" =~ (ERROR|Failed|Timeout|Removed) ]]; then
+            pid_array=("${pid_array[@]/$i}") 
+            invalid_pids+=("$i") 
+        else
+            valid_pids+=("$i")
         fi
     done
-
-    echo "${#pid_array[@]} Pids"
+    echo "Invalid Pids: ${#invalid_pids[@]}"
+    echo "${#valid_pids[@]} Pids"
 }
 
 process_passwords() {
-    local passwords=($(grep -E "'(admin_pass|softdbpass)' => '" "$path_file" | awk -F"'" '{print $(NF-1)}' | sort))
+    local passwords=($(grep -E "'[a-z_]*pass' => '" "$path_file" | awk -F"'" '{print $(NF-1)}' | sort))
+    local html_passwords=($(awk -F'value="' '/id="admin_pass"/ {print $2}' "$path_file" | cut -d '"' -f1))
+
+    passwords+=("${html_passwords[@]}") 
 
     for i in "${passwords[@]}"; do
 
         echo "Password: $i"
         
-        if [[ ${#i} -lt 8 ]]; then
+        if [[ "${#i}" -lt 8 ]]; then
             echo "Password is short"
         elif [[ ! "$i" =~ [A-Z] ]]; then
             echo "Capital letter is absent"
